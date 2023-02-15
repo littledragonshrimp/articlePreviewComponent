50670c263514a48e818403f27e4926a8f68b21c7
Jordan <JDLChang@gmail.com>
2023-02-15 16:24:13 -0600

    2nd commit, adds all files

---
+++ actions.js
@@ -0,0 +1,23 @@
+//const button = document.querySelector("button#click"); //click
+//const canHide = document.querySelector(".canHide");
+//const hiding = document.querySelector(".hidden") //
+//const reveal = document.querySelector(".profile");
+//
+//
+//button.addEventListener("click", function () {
+//    var element = document.getElementById("hidden");
+//    element.classList.add(".show");
+//    console.log("it worked");
+//
+//    canHide.classList.add("hidden");
+//    console.log("canHide now hidden");
+//  });
+
+
+const arrow = document.querySelector("#click");
+const share = document.querySelector(".share");
+
+arrow.addEventListener("click", function () {
+  share.classList.toggle("show");
+  console.log("it works");
+});
